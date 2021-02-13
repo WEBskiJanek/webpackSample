e8baf86bc3f52fd18e80e3dbee3cae831c4ab63d
WEBskiJanek <rudyjanek1991@gmail.com>
2021-02-13 14:23:53 +0100

    Background color with title

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import {sum} from './sum';
+import style from './css/index.scss';
+
+console.log(sum(3,4));
+
+const head = document.getElementById("demo"),
+sumValue = sum(152,4);
+
+head.innerHTML = `Wynik dodawania 152+4 = ${sumValue}`
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+entry: "./src/index.js",
+output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+    },
+
+    devServer:{
+        contentBase: path.join(__dirname,"dist"),
+        port: 9000,
+        watchContentBase: true
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            // use: [
+            //     {loader: "style-loader/url"},
+            //     {loader: "file-loader",
+            //     options: {name: "[name].[ext]"}}
+            //     ]
+            use: ["style-loader", "css-loader", "sass-loader"]
+        },
+        {
+            test:/\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }
+    ]
+    }
+}
